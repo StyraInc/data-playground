beaa1df00a72f3ecfd27e14e56156f19faff6990
Stephan Renatus <stephan@styra.com>
2025-04-04 13:59:20 +0200

    helpers: use @styra/opa

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,3 +1,5 @@
+import { OPAClient } from "npm:@styra/opa@^1.7.9";
+
 export async function putPolicy(opa, id, code, raise = true) {
   const resp = await fetch(`${opa}v1/policies/${id}`, {
     method: "PUT",
@@ -10,22 +12,15 @@ export async function putPolicy(opa, id, code, raise = true) {
   return resp;
 }
 
-export  async function compilePolicy(opa, input, mappings = {}, query = "data.filters.include") {
-  const resp = await fetch(`${opa}v1/compile`, {
-    method: "POST",
-    body: JSON.stringify({
-      input,
-      query,
-      options: {
-        targetSQLTableMappings: {
-          postgresql: mappings,
-        },
-      },
-    }),
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/vnd.styra.sql.postgresql+json"
-    },
-  });
-  return resp;
+export  async function compilePolicy(opa, input, tableMappings = {}, path = "filters/include") {
+  const href = window.location.toString();
+  const u = new URL(href); // TODO(sr): better way?!
+  u.pathname = "";
+  u.search = "";
+  const client = new OPAClient(u.toString());
+  return client.getFilters(path, input,
+    {
+      target: "postgresql",
+      tableMappings,
+    }).catch((e) => ({ errors: e.data$.errors }));
 }
